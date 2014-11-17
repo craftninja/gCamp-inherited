e45a0fdf6f43ebe322cb27426480578c9d6bef34
EmilyPlatzer <emilyplatzer@gmail.com>
2014-11-16 21:35:42 -0700

    Faker data for Projects and related Tasks and Members

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 10.times do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
@@ -15,4 +16,25 @@
     password: "#{first_name}password",
     password_confirmation: "#{first_name}password" }
   )
+end
+
+3.times do
+  project = Project.create(
+    name: "#{Faker::Hacker.noun.capitalize} Project",
+  )
+  6.times do
+    Task.create(
+      description: "#{Faker::Hacker.verb.capitalize} #{project.name}",
+      due_date: Faker::Time.forward(23),
+      complete: [false, false, false, true].sample,
+      project_id: project.id
+    )
+  end
+  4.times do
+    Membership.create(
+      project_id: project.id,
+      user_id: User.all.sample.id,
+      role: [0,0,0,1].sample
+    )
+  end
 end
