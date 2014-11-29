cb535db42ae66604bb9066af95d9894cbe77e289
EmilyPlatzer <emilyplatzer@gmail.com>
2014-11-29 12:29:12 -0700

    Rewrote tasks model spec to be more clear, concise

--- spec/models/tasks_spec.rb
+++ spec/models/tasks_spec.rb
@@ -1,40 +1,32 @@
 require 'rails_helper'
-
+include ActiveSupport::Testing::TimeHelpers
 
 describe Task do
 
-  it 'verifies that task due date is invaild' do
-
+  it 'validates presence of description, due date must not be in past' do
     task = Task.new
     expect(task.valid?).to be(false)
     task.description = 'test'
     expect(task.valid?).to be(true)
     task.due_date = Date.today-7
     expect(task.valid?).to be(false)
-
-  end
-
-  it 'verifies that task date is vaild' do
-
-    task = Task.new
-    expect(task.valid?).to be(false)
-    task.description = 'test'
-    expect(task.valid?).to be(true)
     task.due_date = Date.today+7
     expect(task.valid?).to be(true)
-
   end
 
-  it 'verifies that task date is invaild in edit' do
-
+  it 'validates that past-due task descriptions can be updated' do
+    project = create_project
     task = Task.new
-    expect(task.valid?).to be(false)
-    task.description =
-    expect(task.valid?).to be(false)
-    task.due_date = Date.today+7
+    travel_to(1.day.ago) do
+      task.description = 'old description'
+      task.due_date = Date.today
+      task.project_id = project.id
+      expect(task.valid?).to be(true)
+      task.save
+    end
+    travel_back
+    task.description = 'new description'
     expect(task.valid?).to be(true)
-
-
   end
 
 end
